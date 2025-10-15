14de6a2642edae5ce3c73b0d4c3e1edab11c9b5b
Abdullah Muhammad Idris <abdullah.m.idris1@gmail.com>
2025-10-15 10:35:54 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ class Config:
     DB_PORT = os.getenv("DB_PORT")
     DB_NAME = os.getenv("DB_NAME")
 
-    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}?gssencmode=disable"
 
     DEBUG = False
