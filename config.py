09d4e4d4dad1014fad4c8636b1f56663e5545ba2
Abdullah Muhammad Idris <abdullah.m.idris1@gmail.com>
2025-10-15 21:25:07 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ load_dotenv()
 
 class Config:
     DB_USER = os.getenv("DB_USER")
-    DB_PASS = os.getenv("DB_PASSWORD")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_HOST = os.getenv("DB_HOST")
     DB_PORT = os.getenv("DB_PORT")
     DB_NAME = os.getenv("DB_NAME")
 
-    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}?gssencmode=disable"
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?gssencmode=disable"
 
     DEBUG = False
